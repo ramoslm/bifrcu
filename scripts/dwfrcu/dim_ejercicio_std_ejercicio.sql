3bc7e36cfd9ffec1127ef538c36572a36481f6a5
ramoslm <ramoslautarom@gmail.com>
2014-05-29 20:24:23 -0300

    Correcciones sobre el script la dimensión ejercicio del data warehouse

--- scripts/dwfrcu/dim_ejercicio_std_ejercicio.sql
+++ scripts/dwfrcu/dim_ejercicio_std_ejercicio.sql
@@ -20,8 +20,8 @@ ALTER TABLE dim_ejercicio_std_ejercicio OWNER TO etl_user;
 
 -- PREGUNTAR QUE PONER EN DESCONOCIDO Y EN NO APLICA PARA LA FECHA
 INSERT INTO dim_ejercicio_std_ejercicio(ejercicio_sk, ejercicio_bk, ejercicio_nombre, ejercicio_cierreanual, ejercicio_fechadesde, ejercicio_fechahasta, ejercicio_activo, ejercicio_abierto, ejercicio_fechacongelamiento, ejercicio_ultdiahabil, ejercicio_sort, batch_id)
-VALUES(-1, 'DESC', 'Desconocido', 'D', '', '', 'D', 'D', '', '', 'ZZZZZZZZZZZZZZZZZZZZ', 0);
+VALUES(-1, 'DESC', 'Desconocido', 'D', NULL, NULL, 'D', 'D', NULL, NULL, 'ZZZZZZZZZZZZZZZZZZZZ', 0);
 
 INSERT INTO dim_ejercicio_std_ejercicio(ejercicio_sk, ejercicio_bk, ejercicio_nombre, ejercicio_cierreanual, ejercicio_fechadesde, ejercicio_fechahasta, ejercicio_activo, ejercicio_abierto, ejercicio_fechacongelamiento, ejercicio_ultdiahabil, ejercicio_sort, batch_id)
-VALUES(-2, 'NA', 'No Aplicable', 'D', '', '', 'D', 'D', '', '', 'ZZZZZZZZZZZZZZZZZZZZ', 0);
+VALUES(-2, 'NA', 'No Aplicable', 'D', NULL, NULL, 'D', 'D', NULL, NULL, 'ZZZZZZZZZZZZZZZZZZZZ', 0);
 
