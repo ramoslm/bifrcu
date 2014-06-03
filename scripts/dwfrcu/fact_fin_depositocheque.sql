9ed35d3ee517eb36ed5320266f0b6afbeb9e91cb
ramoslm <ramoslautarom@gmail.com>
2014-06-03 19:52:09 -0300

    Modifique los scripts de la tabla de hecho Deposito Cheque

--- scripts/dwfrcu/fact_fin_depositocheque.sql
+++ scripts/dwfrcu/fact_fin_depositocheque.sql
@@ -3,9 +3,9 @@
   ctabancaria_std_ctabancaria_sk INTEGER,
   mediodepago_std_mediodepago_sk INTEGER,
   ejercicio_std_ejercicio_sk INTEGER,
+  fecha_autorizacion_std_fecha_sk INTEGER,
+  fecha_std_fecha_sk INTEGER,
   observacion TEXT,
-  fecha_autorizacion DATE,
-  fecha DATE,
   procesado VARCHAR(4),
   nro_deposito VARCHAR(15),
   id_deposito VARCHAR(15),
@@ -17,7 +17,9 @@
   CONSTRAINT fk_fact_fin_depositocheque_banco_std_banco FOREIGN KEY (banco_std_banco_sk) REFERENCES dim_banco_std_banco,
   CONSTRAINT fk_fact_fin_depositocheque_ctabancaria_std_ctabancaria FOREIGN KEY (ctabancaria_std_ctabancaria_sk) REFERENCES dim_ctabancaria_std_ctabancaria,
   CONSTRAINT fk_fact_fin_depositocheque_mediodepago_std_mediodepago FOREIGN KEY (mediodepago_std_mediodepago_sk) REFERENCES dim_mediodepago_std_mediodepago,
-  CONSTRAINT fk_fact_fin_depositocheque_ejercicio_std_ejercicio FOREIGN KEY (ejercicio_std_ejercicio_sk) REFERENCES dim_ejercicio_std_ejercicio
+  CONSTRAINT fk_fact_fin_depositocheque_ejercicio_std_ejercicio FOREIGN KEY (ejercicio_std_ejercicio_sk) REFERENCES dim_ejercicio_std_ejercicio,
+  CONSTRAINT fk_fact_fin_depositocheque_fecha_autorizacion_std_fecha FOREIGN KEY (fecha_autorizacion_std_fecha_sk) REFERENCES dim_fecha_std,
+  CONSTRAINT fk_fact_fin_depositocheque_fecha_std_fecha FOREIGN KEY (fecha_std_fecha_sk) REFERENCES dim_fecha_std
 );
 
 ALTER TABLE fact_fin_depositocheque OWNER TO etl_user;
