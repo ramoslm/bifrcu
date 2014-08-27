53404f8c862d88238ed1d824283448f5cbccba81
ramoslm <ramoslautarom@gmail.com>
2014-08-27 11:08:45 -0300

    Diemnsión fuente: etl db fuente a staging y script tabla staging

---
+++ scripts/dwfrcu_staging/s_dim_fuente_std_fuente_src.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE s_dim_fuente_std_fuente_src(
+  fuente_bk VARCHAR(10),
+  fuente_nombre VARCHAR(40),
+  banco_sort character varying(30),
+  batch_id bigint
+);
+
+ALTER TABLE s_dim_fuente_std_fuente_src OWNER TO etl_user;
