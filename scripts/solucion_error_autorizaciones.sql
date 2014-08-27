1ac956c9850efe2229c4e1796614d4b74fe51a39
ramoslm <ramoslautarom@gmail.com>
2014-08-27 11:09:48 -0300

    Script para solucionar el error de las autorizaciones en los reportes Qbe.

---
+++ scripts/solucion_error_autorizaciones.sql
@@ -0,0 +1,5 @@
+﻿-- Con esto se soluciona el problema de que no se gurdan las autorizaciones para los roles de SpagoBI
+-- Ejecutar sobre la BD de metadata, spagobi
+ALTER TABLE sbi_authorizations ALTER COLUMN creation_date SET DEFAULT NOW()::DATE;
+
+ALTER TABLE sbi_authorizations ALTER COLUMN last_change_date SET DEFAULT NOW()::DATE;
