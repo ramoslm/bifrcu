888a26f5e6a47fe3b1630164526f04c88b52638b
ramoslm <ramoslautarom@gmail.com>
2014-08-28 11:24:15 -0300

    ETLs y scripts de creación de tabla de la dimensión códifo económico.

---
+++ scripts/dwfrcu/dim_codeconomico_std_codeconomico.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE dim_codeconomico_std_codeconomico(
+  codeconomico_sk SERIAL NOT NULL,
+  codeconomico_bk VARCHAR(10),
+  codeconomico_nombre VARCHAR(40),
+  codeconomico_sort VARCHAR(30),
+  codeconomico_check VARCHAR(32),
+  batch_id BIGINT,
+  CONSTRAINT pk_dim_codeconomico_std_codeconomico PRIMARY KEY(codeconomico_sk),
+  CONSTRAINT uk_dim_codeconomico_std_codeconomico UNIQUE(codeconomico_bk)
+);
+
+ALTER TABLE dim_codeconomico_std_codeconomico OWNER TO etl_user;
---
+++ scripts/dwfrcu_staging/s_dim_codeconomico_std_codeconomico_src.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE s_dim_codeconomico_std_codeconomico_src(
+  codeconomico_bk VARCHAR(10),
+  codeconomico_nombre VARCHAR(40),
+  codeconomico_sort VARCHAR(30),
+  batch_id BIGINT
+);
+
+ALTER TABLE s_dim_codeconomico_std_codeconomico_src OWNER TO etl_user;
