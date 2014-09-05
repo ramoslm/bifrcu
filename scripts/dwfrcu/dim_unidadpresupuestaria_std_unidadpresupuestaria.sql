abebd649612c904013c6bd500af033166c80ccd8
Tomás Domé <dometj@gmail.com>
2014-09-04 21:44:46 -0300

    Agrego los scripts de las tablas de dwfrcu y de dwfrcu_staging para unidadpresupuestaria.

---
+++ scripts/dwfrcu/dim_unidadpresupuestaria_std_unidadpresupuestaria.sql
@@ -0,0 +1,28 @@
+﻿-- Table: dim_unidadpresupuestaria_std_unidadpresupuestaria
+
+-- DROP TABLE dim_unidadpresupuestaria_std_unidadpresupuestaria;
+
+CREATE TABLE dim_unidadpresupuestaria_std_unidadpresupuestaria
+(
+  unidadpresupuestaria_sk serial NOT NULL,
+  unidadpresupuestaria_unidadpresupuestaria character varying(10),
+  unidadpresupuestaria_subunidadpresupuestaria character varying(10),
+  unidadpresupuestaria_nombre character varying(40),
+  unidadpresupuestaria_sort character varying(30),
+  unidadpresupuestaria_check character varying(32),
+  batch_id bigint,
+  CONSTRAINT pk_dim_unidadpresupuestaria_std_unidadpresupuestaria PRIMARY KEY (unidadpresupuestaria_sk),
+  CONSTRAINT uk_dim_unidadpresupuestaria_std_unidadpresupuestaria UNIQUE (unidadpresupuestaria_unidadpresupuestaria,unidadpresupuestaria_subunidadpresupuestaria)
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE dim_unidadpresupuestaria_std_unidadpresupuestaria
+  OWNER TO etl_user;
+
+INSERT INTO dim_unidadpresupuestaria_std_unidadpresupuestaria(unidadpresupuestaria_sk, unidadpresupuestaria_unidadpresupuestaria, unidadpresupuestaria_subunidadpresupuestaria, unidadpresupuestaria_nombre,unidadpresupuestaria_sort,unidadpresupuestaria_check)
+VALUES (-1, -1, -1, 'Desconocido','DESC', 'ZZZZZZZZZZZZZ');
+
+INSERT INTO dim_unidadpresupuestaria_std_unidadpresupuestaria(unidadpresupuestaria_sk, unidadpresupuestaria_unidadpresupuestaria, unidadpresupuestaria_subunidadpresupuestaria, unidadpresupuestaria_nombre,unidadpresupuestaria_sort,unidadpresupuestaria_check)
+VALUES (-2, -2, -2, 'No Aplica' ,'NA', 'ZZZZZZZZZZZZZ');
---
+++ scripts/dwfrcu_staging/s_dim_unidadpresupuestaria_std_unidadpresupuestaria_src.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE s_dim_unidadpresupuestaria_std_unidadpresupuestaria_src(
+  unidadpresupuestaria_unidadpresupuestaria VARCHAR(10),
+  unidadpresupuestaria_subunidadpresupuestaria VARCHAR(10),
+  unidadpresupuestaria_nombre VARCHAR(40),
+  unidadpresupuestaria_sort character varying(30),
+  batch_id bigint
+);
+
+ALTER TABLE s_dim_unidadpresupuestaria_std_unidadpresupuestaria_src OWNER TO etl_user;
