0dd3e3f467a5691683592a955ee2c39aa8c33a3f
ramoslm <ramoslautarom@gmail.com>
2014-09-13 20:15:51 -0300

    para poder cambiar de branch

--- scripts/dwfrcu_staging/s_fact_fin_gasto_map.sql
+++ scripts/dwfrcu_staging/s_fact_fin_gasto_map.sql
@@ -1,4 +1,6 @@
-﻿CREATE TABLE s_fact_fin_gasto_map(
+﻿-- DROP TABLE s_fact_fin_gasto_map;
+
+CREATE TABLE s_fact_fin_gasto_map(
   rowid INT NOT NULL,
   fuente_std_fuente_sk INT,
   funcion_std_funcion_sk INT,
@@ -7,11 +9,16 @@
   unidadpresupuestaria_std_unidadpresupuestaria_sk INT,
   redprogramatica_std_redprogramatica_sk INT,
   objetogasto_std_objetogasto_sk INT,
-  m_ingreso NUMERIC(16, 2),
+  m_cred_vigente NUMERIC(40, 2),
+  m_cred_original NUMERIC(40, 2),
   m_preventivo NUMERIC(14, 2),
   m_comprimiso NUMERIC(14, 2),
   m_devengado NUMERIC(16, 2),
   m_pagado NUMERIC(16, 2),
+  m_saldo_presupuestario NUMERIC(40, 2),
+  m_preventivo_no_comprometido NUMERIC(40, 2),
+  m_compromiso_no_devengado NUMERIC(40, 2),
+  m_devengado_no_pagado NUMERIC(40, 2),
   nro_recibo INTEGER,
   ejercicio INTEGER,
   anio_reglamentacion SMALLINT,
--- scripts/dwfrcu_staging/s_fact_fin_gasto_src.sql
+++ scripts/dwfrcu_staging/s_fact_fin_gasto_src.sql
@@ -1,14 +1,21 @@
-﻿CREATE TABLE s_fact_fin_gasto_src(
+﻿-- DROP TABLE s_fact_fin_gasto_src;
+
+CREATE TABLE s_fact_fin_gasto_src(
   rowid SERIAL NOT NULL,
   fuente_std_fuente_bk VARCHAR(10),
   funcion_std_funcion_bk VARCHAR(10),
   codeconomico_std_codeconomico_bk VARCHAR(10),
   fecha_std_fecha_bk VARCHAR(25),
-  m_ingreso NUMERIC(16, 2),
+  m_cred_vigente NUMERIC(40, 2),
+  m_cred_original NUMERIC(40, 2),
   m_preventivo NUMERIC(14, 2),
   m_comprimiso NUMERIC(14, 2),
   m_devengado NUMERIC(16, 2),
   m_pagado NUMERIC(16, 2),
+  m_saldo_presupuestario NUMERIC(40, 2),
+  m_preventivo_no_comprometido NUMERIC(40, 2),
+  m_compromiso_no_devengado NUMERIC(40, 2),
+  m_devengado_no_pagado NUMERIC(40, 2),
   nro_recibo INTEGER,
   ejercicio INTEGER,
   anio_reglamentacion SMALLINT,
